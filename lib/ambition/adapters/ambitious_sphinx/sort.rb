235b70df3e2e54e0fe9b1977a0760b48c3b2f5a0
Josh Nichols <josh@technicalpickles.com>
2008-02-18 19:42:41 -0500

    More rdoc. Added specs that sorting should fail.

--- lib/ambition/adapters/ambitious_sphinx/sort.rb
+++ lib/ambition/adapters/ambitious_sphinx/sort.rb
@@ -4,38 +4,32 @@ module Ambition #:nodoc:
       # Allows you to sort queries by particular fields.
       # It is unsure how well this is supported by Ultrasphinx.
       class Sort < Base
-        # >> sort_by { |u| u.age }
-        # => #sort_by(:age)
+        # Not implemented yet.
         def sort_by(method)
           raise "Not implemented yet."
         end
 
-        # >> sort_by { |u| -u.age }
-        # => #reverse_sort_by(:age)
+        # Not implemented yet.
         def reverse_sort_by(method)
           raise "Not implemented yet."
         end
 
-        # >> sort_by { |u| u.profile.name }
-        # => #chained_sort_by(:profile, :name)
+        # Not implemented yet.
         def chained_sort_by(receiver, method)
           raise "Not implemented yet."
         end
 
-        # >> sort_by { |u| -u.profile.name }
-        # => #chained_reverse_sort_by(:profile, :name)
+        # Not implemented yet.
         def chained_reverse_sort_by(receiver, method)
           raise "Not implemented yet."
         end
 
-        # >> sort_by(&:name)
-        # => #to_proc(:name)
+        # Not implemented yet.
         def to_proc(symbol)
           raise "Not implemented yet."
         end
 
-        # >> sort_by { rand }
-        # => #rand
+        # Not implemented yet.
         def rand
           raise "Not implemented yet."
         end
